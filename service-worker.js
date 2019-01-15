2b188df7ddb1d0af6b4eb4b5f2ccba8496afa927
Liquid-Zhangliquan <1063754495@qq.com>
2019-01-15 14:26:25 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-app/precache-manifest.839d7a7c346817ada725da89e4d81019.js"
+  "/react-app/precache-manifest.f5577e23336bc0a65bed8cbbc7c42921.js"
 );
 
 workbox.clientsClaim();
